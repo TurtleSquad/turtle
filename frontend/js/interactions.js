619c20d31f08359e15b69a88765f0231073dcbb5
Andre Dinh <dinh.andre@yahoo.com>
2015-07-02 14:42:01 -0700

    comment out messages added ids to chat.html add class to interactions.comment whole message.js

--- frontend/js/interactions.js
+++ frontend/js/interactions.js
@@ -2,33 +2,67 @@ $(document).ready(function(){
   $(".emjoi").on('click',function(){
     $("div").toggleClass('hidden');
   });
-
-  $("#whoChat").on('click',function(){
-    var people = window.prompt("Who do you want to chat with?");
-    if(people == 'andre'){
-      $('p').empty();
-    }else{
-      alert('Their is no one that name.');
-    }
+  $('.face').on('click', function(){
+    var image = $(this).attr('id');
+    var iconShow = $('#message-text').val(image);
   });
+  // $("#create_room").on('click',function(){
+  //   var room = prompt('Name of Room ?');
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: 'https://warm-escarpment-7619.herokuapp.com/api/threads',
+  //     success: function(data) {
+  //       console.log(data);
+  //       $.each(data.threads, function(i, thread) {
+  //         $.each(thread.messages,function(i,message){
+  //           thread.roomID;
+  //           message.timeStamp;
+  //           message.username;
+  //           message.message;
+  //           if (room == thread.roomID){
+  //             $('#chat-box').append('<p>'+ ' '+ message.username + '[ '+ message.timeStamp + '] : ' + message.message +'<p>');
+  //           }else{
+  //             alert('the room is not available');
+  //           }
+  //         });
+  //       });
+  //     },
+  //     failure: function(err) {
+  //       console.log(err);
+  //     }
+  //   });
+
+
+    // var people = window.prompt("Who do you want to chat with?");
+    // if(people == 'andre'){
+    //   $('p').empty();
+    // }else{
+    //   alert('Their is no one that name.');
+    // }
+
+
+  // });
+
+
+
   $('#logOut').on('click', function(){
     localStorage.clear();
     window.location.href = "index.html";
   });
 
-  setInterval(function() {
-    $.ajax({
-      type: 'GET',
-      url: 'https://warm-escarpment-7619.herokuapp.com/api/threads',
-      success: function(data) {
-        $.each(data.threads, function(i, thread) {
-          $.each(thread.messages,function(i,message){
-            // $('body').append('<div class="hiddenMessage">' + ' '+ thread.roomID +' '+ message.username + ' ' + message.message +'</div>');
-          console.log(thread.roomID +' '+ message.username + ' ' + message.message );
-          });
-        });
-      }
-    });
-}, 1000 * 60 * .2); // every 12 seconds
+//   setInterval(function() {
+//     $.ajax({
+//       type: 'GET',
+//       url: 'https://warm-escarpment-7619.herokuapp.com/api/threads',
+//       success: function(data) {
+//         $.each(data.threads, function(i, thread) {
+//           $.each(thread.messages,function(i,message){
+//             // $('body').append('<div class="hiddenMessage">' + ' '+ thread.roomID +' '+ message.username + ' ' + message.message +'</div>');
+//           console.log(thread.roomID +' '+ message.username + ' ' + message.message );
+//           });
+//         });
+//       }
+//     });
+// }, 1000 * 60 * .2); // every 12 seconds
 
 });
--- frontend/js/message.js
+++ frontend/js/message.js
@@ -2,7 +2,7 @@ $(function() {
   // post on the message
   $.ajax({
     type: 'GET',
-    url: '/api/threads',
+    url: 'https://warm-escarpment-7619.herokuapp.com/api/threads',
     success: function(data) {
       console.log(data);
       $.each(data.threads, function(i, thread) {
@@ -21,26 +21,26 @@ $(function() {
   });
 
   //Post a message to the server
-  var message = {
-    roomID: '1',
-    timeStamp: Date.now(),
-    username: 'Andre',
-    message: 'I am a message'
-  }
-  $.ajax({
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    url: '/api/new_message',
-    success: function(newthread) {
-      console.log(newthread);
-    }
-  });
-
-
-$("li").click(function() {
-  alert( "Handler for .click() called." );
-  // if (this.class == thread.roomID) {
-    // $( "div" ).show();
-  }
+//   var message = {
+//     roomID: '1',
+//     timeStamp: Date.now(),
+//     username: 'Andre',
+//     message: 'I am a message'
+//   }
+//   $.ajax({
+//     type: 'POST',
+//     data: JSON.stringify(message),
+//     contentType: 'application/json',
+//     url: '/api/new_message',
+//     success: function(newthread) {
+//       console.log(newthread);
+//     }
+//   });
+//
+//
+// $("li").click(function() {
+//   alert( "Handler for .click() called." );
+//   // if (this.class == thread.roomID) {
+//     // $( "div" ).show();
+//   // }
 });
