4ed72146fc8d0e5bedc8387b3fe7143e036b2879
Brandon LaFave <lafaveb@gmail.com>
2015-07-05 16:35:56 -0700

    Add hamburger.js

--- frontend/js/hamburger.js
+++ frontend/js/hamburger.js
@@ -1,23 +1,11 @@
 $(function() {
   //init
-  $("#post-one").addClass("collapse");
-  $("#post-two").toggleClass("collapse");
-  $("#post-three").toggleClass("collapse");
+  $("#room").addClass("collapse");
 
-  $("#post-one-heading").on("click", collapseOne);
-  $("#post-two-heading").on("click", collapseTwo);
-  $("#post-three-heading").on("click", collapseThree);
+  $("#room-heading").on("click", collapse);
 
   //Dom not ready code
-  function collapseOne() {
-    $("#post-one").toggle();
-  }
-
-  function collapseTwo() {
-    $("#post-two").toggle();
-  }
-
-  function collapseThree() {
-    $("#post-three").toggle();
+  function collapse() {
+    $("#room").toggle();
   }
 });
