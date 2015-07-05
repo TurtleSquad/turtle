c57ac341aab0e16d9d7a6ca9111274ae16ca7351
Brandon LaFave <lafaveb@gmail.com>
2015-07-05 15:56:35 -0700

    Tweak CSS.

--- frontend/js/interactions.js
+++ frontend/js/interactions.js
@@ -1,15 +1,8 @@
 $(document).ready(function(){
   $(".emjoi").on('click',function(){
-    $("#emjoi-container").toggleClass('hidden');
+    $("div").toggleClass('hidden');
   });
 
-  $("#whoChat").on('click',function(){
-    var people = window.prompt("Who do you want to chat with?");
-    if(people == 'andre'){
-      $('p').empty();
-    }else{
-      alert('Their is no one that name.');
-    }
   });
   $('#logOut').on('click', function(){
     localStorage.clear();
