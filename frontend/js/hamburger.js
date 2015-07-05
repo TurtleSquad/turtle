fd607a65ae2148edf0a249085de428b2be70cd7a
Brandon LaFave <lafaveb@gmail.com>
2015-07-05 16:39:35 -0700

    Fix hidden room button

--- frontend/js/hamburger.js
+++ frontend/js/hamburger.js
@@ -1,11 +1,11 @@
 $(function() {
   //init
-  $("#room").addClass("collapse");
+  $(".room-list").addClass("collapse");
 
   $("#room-heading").on("click", collapse);
 
   //Dom not ready code
   function collapse() {
-    $("#room").toggle();
+    $(".room-list").toggle();
   }
 });
